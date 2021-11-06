ad39e0a62d1f038bcd09d53827a02750953e1ffc
dearLu <lady.barabanovapolina@list.ru>
2021-11-06 16:37:29 +0400

    add models and data

---
+++ Library/Library/Models/BookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    /// <summary>
+    /// 1.2.2 - Класс книги
+    /// </summary>
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public HumanDTO Author { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ Library/Library/Models/HumanDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    /// <summary>
+    /// 1.2.1 - Класс человека
+    /// </summary>
+    public class HumanDTO
+    {
+        public int Id { get; set; }
+        public string Name {get; set;}
+        public string Surname {get; set;}
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
